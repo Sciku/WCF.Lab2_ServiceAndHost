41efe2e0d569de0a53360ea91a8c43b0e33eeea1
Scien <timothy.brostrom@gmail.com>
2016-01-19 11:38:21 +0100

    Skapat Selfservice för BMI
    :hamster:
    :scorpius:

--- WCF.Labb2/Program.cs
+++ WCF.Labb2/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WCF_Service;
 
 namespace WCF.Labb2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service");
+            ServiceHost selfServiceHost = new ServiceHost(typeof(BMIService), baseAddress);
+            try
+            {
+                selfServiceHost.AddServiceEndpoint
+                    (typeof(IBMI),
+                    new WSHttpBinding(),
+                    "BMIService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+
+                selfServiceHost.Description.Behaviors.Add(smb);
+
+                selfServiceHost.Open();
+                Console.WriteLine("The gates are open!");
+            }
+            finally
+            {
+                Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                Console.ReadLine();
+                selfServiceHost.Close();
+            }
         }
     }
 }
--- WCF_Service/BMIService.cs
+++ WCF_Service/BMIService.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace WCF_Service
 {
-    class BMIServiceClass
+    [ServiceContract(Namespace = "WCF_Service")]
+    public interface IBMI
     {
+        [OperationContract]
+        decimal BMI(decimal height, decimal weight);
+    }
+
+    public class BMIService : IBMI
+    {
+        public decimal BMI(decimal height, decimal weight)
+        {
+            Console.WriteLine("Mottaget");
+            return weight / (height * height);
+        }
     }
 }
