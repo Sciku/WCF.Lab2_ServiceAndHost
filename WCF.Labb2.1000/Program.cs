7ec9901003afc3c964412454929ec40445291156
Scien <timothy.brostrom@gmail.com>
2016-01-20 12:25:22 +0100

    Gjort WCF och selfService till ThousandDays

---
+++ WCF.Labb2.1000/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WCF_Service_ThousandDays;
+
+
+namespace WCF.Labb2.ThousandDays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_Service_ThousandDays");
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(ThousandDaysService), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint
+                     (typeof(IThousandDays),
+                     new WSHttpBinding(),
+                     "ThousandDaysService");
+
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+
+                    selfServiceHost.Open();
+                    Console.WriteLine("The gates are open!");
+                    Console.WriteLine("Tryck på ENTER för att stänga tjänsten");
+                    Console.ReadLine();
+
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    selfServiceHost.Close();
+                }
+            }
+        }
+    }
+}
---
+++ WCF_Service_1000/ThousandDaysService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCF_Service_ThousandDays
+{
+    [ServiceContract(Namespace = "WCF_Service_ThousandDays")]
+    public interface IThousandDays
+    {
+        [OperationContract]
+        double days(int year, int month, int day);
+    }
+    public class ThousandDaysService : IThousandDays
+    {
+        public double days(int year, int month, int day)
+        {
+            Console.WriteLine("Mottaget");
+            var today = DateTime.Now;
+            var birthday = new DateTime(year, month, day);
+            var calc = 1000 - (today.Date - birthday.Date).Days % 1000;
+            return calc;
+        }
+    }
+}
