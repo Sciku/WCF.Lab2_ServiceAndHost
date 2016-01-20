0a4624682d968bf9ed55aef888aa28afd13c3480
Scien <timothy.brostrom@gmail.com>
2016-01-20 14:00:33 +0100

    Lagt till service och WCF till Hobby
    :punch: like a :dragon:
    :fist: like a Spjut

---
+++ WCF.Labb2.Hobby/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WCF_Service_Hobby;
+namespace WCF.Labb2.Hobby
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/WCF_service_Hobby");
+
+            using (ServiceHost selfServiceHost = new ServiceHost(typeof(HobbyService), baseAddress))
+            {
+                try
+                {
+                    selfServiceHost.AddServiceEndpoint
+                        (typeof(IHobby), 
+                        new WSHttpBinding(), 
+                        "HobbyService");
+
+                    ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+
+                    selfServiceHost.Description.Behaviors.Add(smb);
+
+                    selfServiceHost.Open();
+                    Console.WriteLine("The gates are open!");
+                    Console.WriteLine("Tryck på ENTER för att avsluta tjänsten");
+                    Console.ReadLine();
+                }
+                catch (CommunicationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    selfServiceHost.Close();
+                }
+            }
+        }
+    }
+}
---
+++ WCF_Service_Hobby/HobbyService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCF_Service_Hobby
+{
+    [ServiceContract(Namespace ="WCF_Service_Hobby")]
+    public interface IHobby
+    {
+        [OperationContract]
+        string WorstMovieTitle(string year);
+
+        [OperationContract]
+        string YearOfMovie(string movieTitle);
+
+    }
+    public class HobbyService : IHobby
+    {
+        public string WorstMovieTitle(string year)
+        {
+            Console.WriteLine("Mottaget");
+            var textFromFile = System.IO.File.ReadAllLines(@"D:\VS projects\WCF.Labb2\WCF_Service_Hobby\WorstMovies.txt");
+            var movieTitle = "";
+
+            foreach (var line in textFromFile)
+            {
+                var split = line.Split('\t');
+
+                if (split.Contains(year))
+                {
+                    movieTitle = split[1];
+
+                }
+            }
+            return movieTitle;
+
+        }
+
+        public string YearOfMovie(string movieTitle)
+        {
+            Console.WriteLine("Mottaget");
+            var textFromFile = System.IO.File.ReadAllLines(@"D:\VS projects\WCF.Labb2\WCF_Service_Hobby\WorstMovies.txt");
+            var year = "";
+
+            foreach (var line in textFromFile)
+            {
+                var split = line.Split('\t');
+
+                if (split.Contains(movieTitle))
+                {
+                    year = "Den som fick en Razzie för sämsta film är: " + "\r\n" + "***" + split[0] + "***";
+
+                }
+            }
+            return year;
+        }
+    }
+}
